81305dcdb1b357c169b9bae69ce311eab1b5eff4
Suman-Rajbhar <changedewind@gmail.com>
2016-12-29 23:15:45 +0600

    Update Home.php

--- Home.php
+++ Home.php
@@ -18,13 +18,32 @@ class Home extends CI_Controller{
     public function new_user_registration()
     {
 
+        // form post data
         $data = array();
         $data['full_name'] = $this->input->post('f_name');
         $data['email'] = $this->input->post('email');
         $data['mobile'] = $this->input->post('mobile');
-        $data['password'] = md5($this->input->post('password'));        
+        $data['password'] = md5($this->input->post('password'));
+        
+        // data insert into model with return insert_id
         $this->b_model->insert_new_blogger($data);
+        
+        // get new registered user info with insert_id
+        $user = $this->b_model->select_blogger_info($id);
+        
+        // mail data here
+        $mail_data=array();            
+        $mail_data['user_name']= $user->full_name;
+        $mail_data['company'] = "Incepta Blog Forum";            
+        $mail_data['mobile'] = $user->mobile;            
+        $mail_data['from_address'] = "incepta@gmail.com";  // mail sender                   
+        $mail_data['to_address'] = $user->email;   // registered user email         
+        $mail_data['subject']="Registration Successful.";   // mail subject         
+        
+        $this->b_model->send_email($mail_data,'activation_email'); // data to blogger model
+        
         redirect("/");
+       
     }
 
     
