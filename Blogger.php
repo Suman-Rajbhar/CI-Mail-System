bb8f729e763ea727de685f12a166a345d9f05385
Suman-Rajbhar <changedewind@gmail.com>
2016-12-29 23:28:06 +0600

    Update Blogger.php

--- Blogger.php
+++ Blogger.php
@@ -8,32 +8,15 @@
 
 class Blogger extends CI_Model{
 
-    public function inser_new_blogger($data)
+    public function insert_new_blogger($data) // data insert in blogers table
     {
         $this->db->insert('blogers', $data);
+        $insert_id = $this->db->insert_id();
+        return  $insert_id; // return last insert id (row id)
     }
 
-    public function check_blogger($email,$password)
-    {
-        $this->db->select('*');
-        $this->db->from('blogers');
-        $this->db->where('email', $email);
-        $this->db->where('password', $password);
-        $query_result = $this->db->get();
-        $result = $query_result->row();
-        return $result;
-    }
-
-    public function select_all_bloggers()
-    {
-        $this->db->select("*");
-        $this->db->from("blogers");
-        $query_result = $this->db->get();
-        $result = $query_result->result();
-        return $result;
-    }
-
-    public function select_blogger_info($id)
+    
+    public function select_blogger_info($id) // get user info from blogers table
     {
         $this->db->select('*');
         $this->db->from('blogers');
@@ -41,140 +24,21 @@ class Blogger extends CI_Model{
         $query_result = $this->db->get();
         $result = $query_result->row();
         return $result;
-    }
-
-    public function select_blog($b_id)
-    {
-        $query = $this->db->select('*')->from('blogs')->where('id', $b_id)->get();
-        $result = $query->row();
-        return $result;
-    }
-
-    public function update_blog_info($id, $data)
-    {
-        $this->db->where('id', $id)->update('blogs', $data);
-    }
-
-    public function insert_new_blog($data)
-    {
-        $this->db->insert('blogs', $data);
-    }
-    public function insert_new_like($b_id)
-    {
-        $data = array(
-            'blog_id' => $b_id,
-            'user_ip' => $_SERVER['REMOTE_ADDR'],
-            'created_at' => date("Y:m:d H:i:s"),
-        );
-        $this->db->insert('blog_like', $data);
-    }
-
-    public function select_all_blogs($per_page, $offset)
-    {
-        $data = array();
-        $this->db->select('*');
-        $this->db->from('blogs');
-        $query = $this->db->get('', $per_page, $offset);
-        foreach ($query->result() as $row)
-            $data[] = $row;
-        return $data;
-    }
-
-    public function select_blogs()
-    {
-        $this->db->select('*');
-        $this->db->from('blogs');
-        $this->db->where('status', 1);
-        $query = $this->db->get();
-        $result  = $query->result();
-        return $result;
-    }
-
-    public function select_blogs_all()
-    {
-        $this->db->select('*');
-        $this->db->from('blogs');
-        $query = $this->db->get();
-        $result  = $query->result();
-        return $result;
-    }
-
-    public function select_blogs_all_user($email)
-    {
-        $this->db->select('*');
-        $this->db->from('blogs');
-        $this->db->where('author', $email);
-        $query = $this->db->get();
-        $result  = $query->result();
-        return $result;
-    }
-
-    public function set_blog_inactive($id)
-    {
-        $data = array(
-            'status' => 0
-        );
-
-        $this->db->where('id', $id);
-        $this->db->update('blogs', $data);
-    }
-
-    public function set_blog_active($id)
-    {
-        $data = array(
-            'status' => 1
-        );
-
-        $this->db->where('id', $id);
-        $this->db->update('blogs', $data);
-    }
-
-    public function count_all_blogs()
-    {
-        return $this->db->count_all("blogs");
     }
 	
-	public function all_info()
+    public function send_email($mail_data,$template_name)
     {
-        $sql = "SELECT * FROM verified_exam_marks";
-        $query_result = $this->db->query($sql);
-        $result = $query_result->result();
-        return $result;
+               
+        $this->load->library('email'); // Email Library 
+        $this->email->set_mailtype('html'); // set type for sending a markup template
+        $this->email->from($mail_data['from_address']); 
+        $this->email->to($mail_data['to_address']);        
+        $this->email->subject($mail_data['subject']);
+        $body = $this->load->view($template_name,$mail_data,true);   // message view template activation_email.php, data into template, boolean     
+        $this->email->message($body);
+        $this->email->send();
+        $this->email->clear();
     }
 
-    public function select_blog_details($id)
-    {
-        $this->db->select('*');
-        $this->db->from('blogs');
-        $this->db->where('id', $id);
-        $query_result = $this->db->get();
-        $result = $query_result->row();
-        return $result;
-    }
-
-    public function select_blog_comments($id)
-    {
-        $query_result = $this->db->select('*')->from('blog_comment')->where('blog_id', $id)->get();
-        $result = $query_result->result();
-        return $result;
-    }
-
-    public function select_blog_likes($id)
-    {
-        $query_result = $this->db->select('*')->from('blog_like')->where('blog_id', $id)->get();
-        $result = $query_result->result();
-        return $result;
-    }
-
-    public function select_blog_user_like($id, $user_ip)
-    {
-        $query_result = $this->db->select('*')->from('blog_like')->where('blog_id', $id)->where('user_ip', $user_ip)->get();
-        $result = $query_result->row();
-        return $result;
-    }
-
-    public function save_a_comment($data)
-    {
-        $this->db->insert('blog_comment', $data);
-    }
+    
 } 
